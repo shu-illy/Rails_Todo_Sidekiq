191ad2db2a172322ced0af2e4dadbed6056d0a1e
Shuhei Iriyama <67177058+shu-illy@users.noreply.github.com>
2021-10-01 23:32:39 +0900

    フォーム作成

--- app/controllers/my_tasks_controller.rb
+++ app/controllers/my_tasks_controller.rb
@@ -1,2 +1,19 @@
 class MyTasksController < ApplicationController
+  def index
+    @my_tasks = MyTask.order(created_at: "DESC").all
+  end
+
+  def show
+    id = params[:id]
+    @my_task = MyTask.find(id)
+  end
+
+  def create
+    my_task = MyTask.new
+    my_task.title = params[:title]
+    my_task.description = params[:description]
+    my_task.public = params[:public]
+    my_task.save
+    redirect_to my_tasks_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'my_tasks', to: 'my_tasks#list'
-  get 'my_tasks/:id', to: 'my_tasks#show'
-  post 'my_tasks', to: 'my_tasks#create'
+  resources :my_tasks, only: [:index, :show, :create]
 end
